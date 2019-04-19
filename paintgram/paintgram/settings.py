5c417b6846638b5fcb3f8b0cddff6fc0bd224d38
akwil05 <bekzatsolton@gmail.com>
2019-04-19 19:37:11 +0600

    initial commit

--- paintgram/paintgram/settings.py
+++ paintgram/paintgram/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cqoy7=30%va0w^*4jk@a*@i=iw%9&&o*s+)wf4=(jd#&n1j*@y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -75,17 +75,22 @@ WSGI_APPLICATION = 'paintgram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'paintgram',
+#         'USER': 'root',
+#         'PASSWORD': '1605bekzat',
+#         'HOST': '',
+#         'PORT': '',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'paintgram',
-        'USER': 'root',
-        'PASSWORD': '1605bekzat',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
