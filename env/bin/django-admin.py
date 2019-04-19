976ba9e982084de1837dd3797c7b7b78c6702ff1
Alymbekov <maxim.makarov.1997@mail.ru>
2019-04-19 20:03:21 +0600

    modified index file

--- env/bin/django-admin.py
+++ env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/bekzat/Desktop/paintgram/paintgram/env/bin/python3
+#!/home/dastan/Desktop/paintgram_project/env/bin/python3
 from django.core import management
 
 if __name__ == "__main__":
